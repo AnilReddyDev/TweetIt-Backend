224bfd803f571aa2bdd64ce3ef87e04c9264b7f0
AnilReddyDev <konyalaanilreddy00@gmail.com>
2023-11-30 22:54:17 +0530

    Added CORS middleware

--- server.js
+++ server.js
@@ -10,10 +10,25 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({
-    credentials:true,
-    origin:'http://192.168.0.109:5173'
-}));
+
+// CORS middleware configuration
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://192.168.0.109:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+  
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
+
+// app.use(cors({
+//     credentials:true,
+//     origin:'http://192.168.0.109:5173'
+// }));
 
 app.use('/api/tweet',require('./Routes/tweetRoutes'))
 app.use('/api/user', require('./Routes/userRoutes'))
